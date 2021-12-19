7c142dd076f802ebed88b53da24153c848550b07
Tombatron <tommy.hanks@gmail.com>
2021-12-19 15:44:11 -0500

    Initial skeleton for day 6 in place.

--- src/day_06.rs
+++ src/day_06.rs
@@ -1,8 +1,17 @@
+pub fn simulate_fish(days: usize, initial_state: Vec<usize>) -> usize {
+    0
+}
+
 #[cfg(test)]
 mod tests {
-    
+    use super::simulate_fish;
+
     #[test]
     fn part_one_sample_input_works(){
+        let sample_input = vec![3,4,3,1,2];
 
+        let result = simulate_fish(80, sample_input);
+
+        assert_eq!(5934, result);
     }
 }
