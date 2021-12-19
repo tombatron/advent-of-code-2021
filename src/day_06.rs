0c3b5fff0543f44d6708c5687f04ede7eadc71d2
Tombatron <tommy.hanks@gmail.com>
2021-12-19 15:55:05 -0500

    Day 6 with sample input done.

--- src/day_06.rs
+++ src/day_06.rs
@@ -1,5 +1,26 @@
 pub fn simulate_fish(days: usize, initial_state: Vec<usize>) -> usize {
-    0
+    let mut current_day = 0;
+
+    let mut current_state = initial_state;
+
+    while current_day < days {
+        let mut next_state = Vec::new();
+
+        for fish in current_state {
+            if let 1.. = fish {
+                next_state.push(fish - 1)
+            } else {
+                next_state.push(6);
+                next_state.push(8);
+            }
+        }
+
+        current_state = next_state;
+
+        current_day += 1;
+    }
+
+    current_state.len()
 }
 
 #[cfg(test)]
@@ -7,8 +28,8 @@ mod tests {
     use super::simulate_fish;
 
     #[test]
-    fn part_one_sample_input_works(){
-        let sample_input = vec![3,4,3,1,2];
+    fn part_one_sample_input_works() {
+        let sample_input = vec![3, 4, 3, 1, 2];
 
         let result = simulate_fish(80, sample_input);
 
